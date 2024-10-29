2f4629d3ce67f59def0e9b58ef96c091562ba42b
hotcool8678 <1343203107@qq.com>
2024-10-29 16:51:34 +0800

    d

---
+++ src/main/java/com/cosmos/coslog/CoslogApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos.coslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoslogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cosmos/coslog/CoslogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmos.coslog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoslogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
